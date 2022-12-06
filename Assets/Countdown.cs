07148aad01bc12f1c6993bd15c5a4b8fb6344675
benpanski <benpanski@gmail.com>
2022-12-07 01:48:43 +0200

    asd

--- Assets/Countdown.cs
+++ Assets/Countdown.cs
@@ -15,7 +15,7 @@ public class Countdown : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (countdownanim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f )
+        if (countdownanim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95f )
         {
             Cat.SetActive(true);
             Destroy(gameObject);
