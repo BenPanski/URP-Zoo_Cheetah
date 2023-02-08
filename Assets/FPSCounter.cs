4f23e7a6163944ac3c35fa4cde861e7a785c7e00
benpanski <benpanski@gmail.com>
2023-02-08 16:03:38 +0200

    fps

--- Assets/FPSCounter.cs
+++ Assets/FPSCounter.cs
@@ -1,34 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FPSCounter : MonoBehaviour
 {
-    [SerializeField] List<Text> FpsCounters;
-    // Start is called before the first frame update
-    float frameCount = 0;
-    float nextUpdate = 0.0f;
-    float fps = 0.0f;
-    float updateRate = 4.0f;  // 4 updates per sec.
+    float deltaTime = 0.0f;
+    int fps = 0;
+    [SerializeField] List<Text> fpsTexts;
 
-    void Start()
-    {
-        nextUpdate = Time.time;
-    }
+ 
 
     void Update()
     {
-        frameCount++;
-        if (Time.time > nextUpdate)
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        fps = (int)(1.0f / deltaTime);
+
+        foreach (Text fpsText in fpsTexts)
         {
-            nextUpdate += 1.0f / updateRate;
-            fps = frameCount * updateRate;
-            frameCount = 0;
-        }
-        foreach (var item in FpsCounters)
-        {
-            item.text = "FPS: " + fps.ToString("F2");
+            fpsText.text = fps.ToString();
         }
     }
 }
