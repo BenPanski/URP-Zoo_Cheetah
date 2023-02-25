d576461a83c6cd04d270f66df80f048324bdffc0
benpanski <benpanski@gmail.com>
2023-02-25 12:46:44 +0200

    Point objects now stick to the ground

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -9,10 +9,19 @@ public class Point : MonoBehaviour
     [SerializeField] public bool stopHere = false;
     [Range(1, 50)]
     [SerializeField] public float waitHereForSec;
+    public LayerMask layerMask;
     internal Vector3 PointPosition;
-
+     float raycastDistance = 40f;
     private void Awake()
     {
+
+        // Shoot a raycast downwards from this object's position
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, layerMask))
+        {
+            // Change the Y position of this object's transform to be 7 units above the hit point
+            transform.position = new Vector3(hit.point.x, hit.point.y , hit.point.z);//+ 6.936f
+        }
         PointPosition = gameObject.transform.position;
     }
 
